fdd48e8571e719100aa44f515732bac871408a0b
Jack Reed <phillipjreed@gmail.com>
2016-11-23 10:00:37 -0500

    enhance seeds to work with other image extensions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-image_files = Rails.root.join(Settings.IMAGE_PATH, '*.jpg')
+image_files = Rails.root.join(Settings.IMAGE_PATH, '*.{jpg, tif, tiff, png}')
 
 Dir.glob(image_files).map { |path| Image.from_file_path(path) }
