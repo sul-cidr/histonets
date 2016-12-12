13a4aac797df9325e7686c7e239ace6aa07d91a5
Jack Reed <phillipjreed@gmail.com>
2016-12-12 15:33:40 -0500

    set a specific test port

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -12,6 +12,7 @@ require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 Capybara.javascript_driver = :poltergeist
+Capybara.server_port = 1337
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
