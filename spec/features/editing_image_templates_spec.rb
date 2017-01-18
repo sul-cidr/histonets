63081f9a39e09d08621b54a7961b6fbc9fbd532d
Scott Bailey <csb5t@virginia.edu>
2017-01-18 12:16:14 -0800

    Hook up form for match options on edit template view

--- spec/features/editing_image_templates_spec.rb
+++ spec/features/editing_image_templates_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe 'Create image templates', type: :feature, js: true do
     check 'collection_template_auto_clean'
     click_button 'Next Step'
   end
-  it 'after creating image_templates sends to edit them' do
+  it 'after creating image_templates, creates an edit page for each' do
     click_button 'Add template of cropped area'
     click_button 'Add template of cropped area'
     click_button 'Next Step'
@@ -23,5 +23,27 @@ RSpec.describe 'Create image templates', type: :feature, js: true do
       click_button 'Next Step'
     end
     expect(page).to have_css 'h1', text: 'Create Image Paths'
+  end
+  describe 'edit image form' do
+    before do
+      click_button 'Add template of cropped area'
+      click_button 'Next Step'
+    end
+    it 'all inputs are disabled' do
+      expect(page).to have_css 'input[type="range"][disabled]', count: 3
+    end
+    it 'input defaults are correct' do
+      expect(page).to have_xpath '//input[@id="'\
+        'image_template_match_options_threshold"][@value=80]'
+      expect(page).to have_xpath '//input[@id="'\
+        'image_template_match_options_elasticity"][@value=0]'
+      expect(page).to have_xpath '//input[@id="'\
+        'image_template_match_options_rotation"][@value=0]'
+    end
+    it 'enabling threshold sends through option' do
+      find('[for="image_template_match_options_threshold_enabled"]').click
+      click_button 'Next Step'
+      expect(ImageTemplate.last.match_options).to eq('threshold' => '80')
+    end
   end
 end
