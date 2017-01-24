483b56d123619726462cf6d8bfd1515d928e20c9
Scott Bailey <csb5t@virginia.edu>
2017-01-24 13:53:34 -0800

    Redirect back to the image creation view instead of continuing in the editing steps

--- app/controllers/image_templates_controller.rb
+++ app/controllers/image_templates_controller.rb
@@ -26,10 +26,7 @@ class ImageTemplatesController < ApplicationController
     @image_template.destroy
     respond_to do |format|
       format.html do
-        redirect_to collection_template_build_path(
-          @image_template.collection_template,
-          'create_image_templates'
-        ), notice: 'Deleted'
+        return_to_image_template_builder
       end
       format.json { head :no_content }
     end
