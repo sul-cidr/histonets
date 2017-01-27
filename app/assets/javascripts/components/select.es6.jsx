e6b405e015151e1476a50bf3c42fc7c2cc336479
Scott Bailey <csb5t@virginia.edu>
2017-01-27 13:00:07 -0800

    Restore match options on edit template form, fixing #134

--- app/assets/javascripts/components/select.es6.jsx
+++ app/assets/javascripts/components/select.es6.jsx
@@ -1,4 +1,15 @@
 class Select extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: props.selected };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
   render() {
     if (!this.props) {
       return null;
@@ -9,6 +20,8 @@ class Select extends React.Component {
         disabled={!this.props.enabled}
         id={this.props.customLabel}
         className="form-control"
+        value={this.state.value}
+        onChange={this.handleChange}
       >
         { this.props.options.map(
           option => <option value={option.toLowerCase()} key={Math.random()}>{option}</option>,
@@ -23,6 +36,7 @@ Select.propTypes = {
   enabled: React.PropTypes.bool,
   fieldName: React.PropTypes.string,
   options: React.PropTypes.arrayOf(React.PropTypes.string),
+  selected: React.PropTypes.string,
 };
 
 Select.defaultProps = {
