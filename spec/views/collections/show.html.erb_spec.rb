e01702a3c26e08757cde5360af407b23d40c2c34
Scott Bailey <csb5t@virginia.edu>
2017-06-13 16:27:36 -0700

    Add tests to cover collection controller and views

---
+++ spec/views/collections/show.html.erb_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'collections/show', type: :view do
+  let(:valid_attributes) do
+    {
+      name: 'Test collection',
+      description: 'Test description'
+    }
+  end
+  it 'renders title as h1' do
+    @collection = Collection.create! valid_attributes
+    render
+    expect(rendered).to have_css 'h1', text: 'Test collection'
+  end
+end
