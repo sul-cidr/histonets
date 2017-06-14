4312ce987f4af0df463a6256a37a58d02e83275f
Scott Bailey <csb5t@virginia.edu>
2017-06-14 15:35:56 -0700

    Add tests for the header partial

---
+++ spec/views/shared/_header.html.erb_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe 'shared/_header', type: :view do
+  it 'renders the brand' do
+    render
+    expect(rendered).to have_css 'a.brand', text: 'Histonets'
+  end
+  it 'renders the nav links' do
+    render
+    expect(rendered).to have_css 'div.nav-item'
+    expect(rendered).to have_css 'a.nav-link'
+  end
+end
