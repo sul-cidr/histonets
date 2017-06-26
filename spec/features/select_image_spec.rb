428c0576ac9dba6d41d266820cfa4a6b6c8433ae
Scott Bailey <csb5t@virginia.edu>
2017-06-26 09:13:59 -0700

    Add basic test for select form

---
+++ spec/features/select_image_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Selecting an image for a template', type: :feature, js: true do
+  before do
+    image = create(:image, file_name: 'eddie.jpg')
+    create(:collection, images: [image])
+  end
+  it 'renders the select form' do
+    visit new_collection_template_path
+    click_link 'Build a Collection Template'
+    click_button 'Next Step'
+    expect(page).to have_content 'Select an Image'
+    expect(page).to have_css 'form.edit_collection_template'
+    expect(page).to have_css 'select#image-select'
+  end
+end
