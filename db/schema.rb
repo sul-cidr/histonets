e0ab0b404ac221385acd342021e99dfb137c6d8d
Jack Reed <phillipjreed@gmail.com>
2017-06-30 11:25:14 -0400

    Rails 5.1 changed the schema.rb spacing

--- db/schema.rb
+++ db/schema.rb
@@ -13,26 +13,26 @@
 ActiveRecord::Schema.define(version: 20170123193743) do
 
   create_table "collection_templates", force: :cascade do |t|
-    t.integer  "collection_id"
-    t.text     "crop_bounds"
-    t.text     "image_clean"
-    t.text     "image_paths"
-    t.text     "image_graph"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-    t.integer  "image_id"
-    t.boolean  "auto_clean"
-    t.text     "image_matches"
+    t.integer "collection_id"
+    t.text "crop_bounds"
+    t.text "image_clean"
+    t.text "image_paths"
+    t.text "image_graph"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "image_id"
+    t.boolean "auto_clean"
+    t.text "image_matches"
     t.index ["collection_id"], name: "index_collection_templates_on_collection_id"
     t.index ["image_id"], name: "index_collection_templates_on_image_id"
   end
 
   create_table "collections", force: :cascade do |t|
-    t.string   "name"
-    t.text     "description"
-    t.integer  "year"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string "name"
+    t.text "description"
+    t.integer "year"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "collections_images", id: false, force: :cascade do |t|
@@ -43,27 +43,27 @@ ActiveRecord::Schema.define(version: 20170123193743) do
   end
 
   create_table "histograms", force: :cascade do |t|
-    t.binary   "histogram"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
-    t.string   "histogramable_type"
-    t.integer  "histogramable_id"
+    t.binary "histogram"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "histogramable_type"
+    t.integer "histogramable_id"
     t.index ["histogramable_type", "histogramable_id"], name: "index_histograms_on_histogramable_type_and_histogramable_id"
   end
 
   create_table "image_templates", force: :cascade do |t|
-    t.integer  "collection_template_id"
-    t.string   "image_url"
-    t.binary   "mask"
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-    t.boolean  "status"
-    t.text     "match_options"
+    t.integer "collection_template_id"
+    t.string "image_url"
+    t.binary "mask"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "status"
+    t.text "match_options"
     t.index ["collection_template_id"], name: "index_image_templates_on_collection_template_id"
   end
 
   create_table "images", force: :cascade do |t|
-    t.string   "file_name"
+    t.string "file_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["file_name"], name: "index_images_on_file_name", unique: true
