5ce34b3bfcb95f54b6b45f6794b8b756514f5777
Scott Bailey <csb5t@virginia.edu>
2017-07-03 14:00:34 -0700

    Implement create image path view with webpack

---
+++ app/javascript/packs/create_image_paths.jsx
@@ -0,0 +1,18 @@
+import ReactDOM from 'react-dom';
+import LeafletIiif from '../components/leaflet_iiif';
+import Histogram from '../components/histogram';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const node = document.getElementById('image-path-image');
+  const data = JSON.parse(node.getAttribute('data'));
+  ReactDOM.render(<LeafletIiif {...data} />, node);
+
+  const histograms = document.getElementsByClassName('histogram');
+
+  for (let i = 1; i < histograms.length; i += 1) {
+    const nodeId = `image-path-histogram-${String(i)}`;
+    const histogramNode = document.getElementById(nodeId);
+    const dataHistogram = JSON.parse(histogramNode.getAttribute('data'));
+    ReactDOM.render(<Histogram {...dataHistogram} />, histogramNode);
+  }
+});
