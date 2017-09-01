433e4363beb2415d92a14d93c87042cbdb128775
Scott Bailey <csb5t@virginia.edu>
2017-09-01 12:09:46 -0700

    Add further tests for build graph feature

--- spec/features/build_graph_spec.rb
+++ spec/features/build_graph_spec.rb
@@ -6,20 +6,21 @@ RSpec.describe 'Build graph', type: :feature, js: true do
   before do
     ActiveJob::Base.queue_adapter = :inline
     image = create(:image, file_name: 'small_map.jpg')
-    create(:collection, images: [image])
+    create(:collection,
+           images: [image],
+           palette: [[0, 0, 0], [255, 255, 255]])
     visit collections_path
     click_button 'Create collection template'
     click_button 'Next Step'
     click_button 'Next Step'
     check 'collection_template_auto_clean'
     click_button 'Next Step'
-    # Click somewhere on the map to zoom in
+    find('#map').double_click
     find('#map').double_click
     click_button 'Add template of cropped area'
     click_button 'Next Step'
     click_button 'Next Step'
-    expect(page).to have_css '.histogram-color', count: 2
-    first('.media-body').click
+    all('.media-body')[1].click
     click_button 'Next Step'
     click_button 'Next Step'
   end
@@ -29,5 +30,19 @@ RSpec.describe 'Build graph', type: :feature, js: true do
   it 'Should display the results of the post process step' do
     expect(page).to have_css 'h1', text: 'Build the graph'
     expect(page).to have_css '#map'
+  end
+  it 'Should display the form for the graph building step' do
+    meth_finder = 'select[name="collection_template[graph]'\
+      '[simplification-method]"] option'
+    format_finder = 'select[name="collection_template[graph]'\
+      '[format]"] option'
+    expect(page).to have_css 'select', count: 2
+    expect(page).to have_css meth_finder, count: 2
+    expect(page).to have_css format_finder, count: 5
+    expect(page).to have_css 'input[type="range"]', count: 1
+  end
+  it 'clicks through the next step without error' do
+    ActiveJob::Base.queue_adapter = :test
+    click_button 'Next Step'
   end
 end
